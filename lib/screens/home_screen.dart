0637933d44456c335b57875a4dd9b152139f6567
Thaanu Perera <thaanup@gmail.com>
2021-04-28 03:20:09 +0530

    Add carousel slider

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:smoothie_app_ui/widgets/carousel_slider.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  //* Background Color list
+  List<List<Color>> colorList = [
+    [
+      Color(0x88ccffdd),
+      Color(0x55ccffdd),
+      Color(0xeeffffff),
+      Colors.white,
+    ],
+    [
+      Color(0x66d8a6ff),
+      Color(0x55d8a6ff),
+      Color(0xeeffffff),
+      Colors.white,
+    ],
+    [
+      Color(0x66ffabab),
+      Color(0x44ffabab),
+      Color(0xeeffffff),
+      Colors.white,
+    ]
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ValueListenableBuilder<int>(
+        valueListenable: carouselIndex,
+        builder: (BuildContext context, int value, Widget child) {
+          return Stack(
+            children: [
+              //* Background Image
+              Container(
+                height: double.infinity,
+                child: Opacity(
+                  opacity: 0.20,
+                  child: Image.asset(
+                    'lib/assets/background.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              //* Gradient
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                alignment: Alignment.topLeft,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: colorList[value],
+                  ),
+                ),
+                //* Header
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.fromLTRB(25, 50, 25, 0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          //* Topic
+                          Text(
+                            'Select your smoothie',
+                            style: TextStyle(
+                              fontFamily: 'sf',
+                              fontSize: 24,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          //* Add to basket icon
+                          Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                              boxShadow: [
+                                new BoxShadow(
+                                  color: Colors.black26,
+                                  offset: new Offset(3.0, 3.0),
+                                  blurRadius: 8.0,
+                                  spreadRadius: 1,
+                                )
+                              ],
+                            ),
+                            child: Image.asset(
+                              'lib/assets/icons/shopping-basket.png',
+                              height: 18,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    //* Carousel Slider
+                    Flexible(
+                      child: CarouselSlider(),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/widgets/carousel_slider.dart
@@ -0,0 +1,435 @@
+import 'package:flutter/material.dart';
+
+ValueNotifier<int> carouselIndex = ValueNotifier<int>(0);
+
+class CarouselSlider extends StatefulWidget {
+  @override
+  _CarouselSliderState createState() => _CarouselSliderState();
+}
+
+class _CarouselSliderState extends State<CarouselSlider> {
+  PageController controller;
+
+  int currentpage = 0;
+  List<Color> colors = [Colors.green[200], Colors.blue[200], Colors.red[200]];
+  @override
+  initState() {
+    super.initState();
+    controller = PageController(
+      initialPage: currentpage,
+      keepPage: false,
+      viewportFraction: 0.85,
+    );
+  }
+
+  // @override
+  // dispose() {
+  //   controller.dispose();
+  //   super.dispose();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 330,
+      alignment: Alignment.topCenter,
+      child: PageView.builder(
+          onPageChanged: (value) {
+            setState(() {
+              currentpage = value;
+            });
+            carouselIndex.value = value;
+          },
+          controller: controller,
+          itemBuilder: (context, index) => cardBuilder(index)),
+    );
+  }
+
+  cardBuilder(int index) {
+    return AnimatedBuilder(
+        animation: controller,
+        builder: (context, child) {
+          double value = 1.0;
+          if (controller.position.haveDimensions) {
+            value = controller.page - index;
+            value = (1 - (value.abs() * .2)).clamp(0.0, 1.0);
+          }
+          return Container(
+            // height: 120,
+            // color: Colors.red,
+            child: Center(
+              child: SizedBox(
+                height: Curves.easeOut.transform(value) * 300,
+                width: Curves.easeOut.transform(value) * 500,
+                child: child,
+              ),
+            ),
+          );
+        },
+        child: carouselWidgets[index]);
+  }
+
+  List<Widget> carouselWidgets = [
+    //* Product 1 -----------------------------------------------------------------------------------------
+    Container(
+      width: 600,
+      margin: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.fromLTRB(26, 20, 0, 0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          new BoxShadow(
+            color: Colors.green[200],
+            offset: new Offset(3.0, 3.0),
+            blurRadius: 20.0,
+            spreadRadius: -10,
+          )
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 8),
+                  //* Product Topic
+                  Text(
+                    'Kiwi & Mint Smoothie',
+                    style: TextStyle(
+                      fontFamily: 'sf',
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  //* Product Dexcription
+                  Container(
+                    width: 140,
+                    child: Text(
+                      'Kiwi & mint-flavored juice and real kiwi fruit pieces shaken with ice.',
+                      style: TextStyle(
+                        fontFamily: 'sf',
+                        fontSize: 16,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 37),
+                  //* Product Price
+                  Text(
+                    'Rs. 680.00',
+                    style: TextStyle(
+                      fontFamily: 'sf',
+                      fontSize: 16,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  //* Buy Now Button
+                  Container(
+                    margin: EdgeInsets.zero,
+                    padding: EdgeInsets.zero,
+                    decoration: BoxDecoration(
+                      // color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        new BoxShadow(
+                          color: Colors.green[300],
+                          offset: new Offset(1.5, 1.5),
+                          blurRadius: 8.0,
+                          spreadRadius: -4,
+                        )
+                      ],
+                    ),
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.fromLTRB(12, 5, 12, 5),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        primary: Colors.grey,
+                        backgroundColor: Color(0xff04b56e),
+                        elevation: 0,
+                      ),
+                      child: Text(
+                        'Buy Now',
+                        style: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              //* Product Image
+              Container(
+                padding: EdgeInsets.only(left: 94, top: 10),
+                child: Image.asset(
+                  'lib/assets/product-1.png',
+                  height: 220,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    ),
+    //* Product 2 -----------------------------------------------------------------------------------------
+    Container(
+      width: 600,
+      margin: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.fromLTRB(26, 20, 0, 0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          new BoxShadow(
+            color: Colors.purple[200],
+            offset: new Offset(3.0, 3.0),
+            blurRadius: 20.0,
+            spreadRadius: -10,
+          )
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 8),
+                  //* Product Topic
+                  Text(
+                    'Mix-Berry Smoothie',
+                    style: TextStyle(
+                      fontFamily: 'sf',
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  //* Product Description
+                  Container(
+                    width: 140,
+                    child: Text(
+                      'Mix-berry flavored juice and real blueberry pieces shaken with ice.',
+                      style: TextStyle(
+                        fontFamily: 'sf',
+                        fontSize: 16,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 34),
+                  //* Product Price
+                  Text(
+                    'Rs. 740.00',
+                    style: TextStyle(
+                      fontFamily: 'sf',
+                      fontSize: 16,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  //* Buy Now button
+                  Container(
+                    margin: EdgeInsets.zero,
+                    padding: EdgeInsets.zero,
+                    decoration: BoxDecoration(
+                      // color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        new BoxShadow(
+                          color: Colors.purple[200],
+                          offset: new Offset(1.5, 1.5),
+                          blurRadius: 8.0,
+                          spreadRadius: -4,
+                        )
+                      ],
+                    ),
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.fromLTRB(12, 5, 12, 5),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        primary: Colors.grey,
+                        backgroundColor: Color(0xff6100ab),
+                        elevation: 0,
+                      ),
+                      child: Text(
+                        'Buy Now',
+                        style: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              //* Product Image
+              Container(
+                padding: EdgeInsets.only(left: 94, top: 20),
+                child: Image.asset(
+                  'lib/assets/product-2.png',
+                  height: 193,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    ),
+    //* Product 3 -----------------------------------------------------------------------------------------
+    Container(
+      width: 600,
+      margin: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.fromLTRB(26, 20, 0, 0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          new BoxShadow(
+            color: Colors.red[200],
+            offset: new Offset(3.0, 3.0),
+            blurRadius: 20.0,
+            spreadRadius: -10,
+          )
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 8),
+                  //* Product Topic
+                  Text(
+                    'Strawberry Smoothie',
+                    style: TextStyle(
+                      fontFamily: 'sf',
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  //* Product Description
+                  Container(
+                    width: 140,
+                    child: Text(
+                      'Strawberry-flavored juice and real strawberry pieces shaken with ice.',
+                      style: TextStyle(
+                        fontFamily: 'sf',
+                        fontSize: 16,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 34),
+                  //* Product Price
+                  Text(
+                    'Rs. 580.00',
+                    style: TextStyle(
+                      fontFamily: 'sf',
+                      fontSize: 16,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  //* Buy now button
+                  Container(
+                    margin: EdgeInsets.zero,
+                    padding: EdgeInsets.zero,
+                    decoration: BoxDecoration(
+                      // color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        new BoxShadow(
+                          color: Colors.red[200],
+                          offset: new Offset(1.5, 1.5),
+                          blurRadius: 8.0,
+                          spreadRadius: -4,
+                        )
+                      ],
+                    ),
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.fromLTRB(12, 5, 12, 5),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        primary: Colors.grey,
+                        backgroundColor: Color(0xffb00015),
+                        elevation: 0,
+                      ),
+                      child: Text(
+                        'Buy Now',
+                        style: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              //* Product Image
+              Container(
+                padding: EdgeInsets.only(left: 105, top: 20),
+                child: Image.asset(
+                  'lib/assets/product-3.png',
+                  height: 210,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    ),
+    //* Product 4 -----------------------------------------------------------------------------------------
+    Container(
+      width: 600,
+      margin: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          new BoxShadow(
+            color: Colors.yellow[200],
+            offset: new Offset(3.0, 3.0),
+            blurRadius: 20.0,
+            spreadRadius: -10,
+          )
+        ],
+      ),
+    ),
+  ];
+}
